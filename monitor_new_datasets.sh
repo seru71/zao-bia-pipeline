b6be383f4440a070575b6a0d6dc248c391bc47b8
seru71 <pawel.sztromwasser@gmail.com>
2017-07-27 08:11:47 +0200

    added logging to the monitoring script

--- monitor_new_datasets.sh
+++ monitor_new_datasets.sh
@@ -8,17 +8,18 @@ DIR=$1
 
 # Constants
 REPO=https://github.com/seru71/zao-bia-pipeline.git
+LOG_FILE=/tmp/monitor_new_dataset.log
 
+# Check if new runfolder exists, and if the run is complete (marked with RTACommplete.txt)
+# Looks for file RTAComplete with modification date <1h from now
 
-# Check if new folders exist, and if the run is complete
-# Looks for file RTAComplete with mod date <1h from now
-
-touch /tmp/monitor_new_dataset.stamp
 new=`find $DIR/RTAComplete.txt -mmin -60`
 
-if [ ! -z "$new" ]; then
-	export RUN_FOLDER=`dirname $new`
-	export RUN_ID=`basename $RUN_FOLDER`
+echo -n `date -Iseconds`" - found: ${new}" >> ${LOG_FILE}
+if [ ! -z "${new}" ]; then
+	export RUN_FOLDER=`dirname ${new}`
+	export RUN_ID=`basename ${RUN_FOLDER}`
+	echo "Exporting RUN_FOLDER [${RUN_FOLDER}] and RUN_ID [${RUN_ID}]" >> ${LOG_FILE}
 fi
 
 # If the runfolder is complete, create pipeline config and start the pipeline
@@ -26,13 +27,18 @@ if [ ! -z $RUN_ID ]; then
 
 	# Create pipeline config file
 	git clone ${REPO} ${WORK_DIR}/${RUN_ID}/pipeline
-	sed 's///g' ${WORK_DIR}/${NEW_RUN}/pipeline/bia_pipeline.config.template > ${WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.config
-
-	# Start the pipeline
-	{WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.py \
+#	sed 's///g' ${WORK_DIR}/${NEW_RUN}/pipeline/bia_pipeline.config.template > ${WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.config
+	cp ${WORK_DIR}/${NEW_RUN}/pipeline/bia_pipeline.config.template > ${WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.config
+	
+	cmd='{WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.py \
 		-s ${WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.config \
 		--run_folder ${NEW_RUN} \
 		-t complete_run \
-		-j8 -vvv &> {WORK_DIR}/${RUN_ID}/pipeline_`date -Iseconds`.err
+		-j8 -vvv &> {WORK_DIR}/${RUN_ID}/pipeline_`date -Iseconds`.err'
+
+	echo -e "Starting the pipeline with command:\n${cmd}" >> ${LOG_FILE}
+
+	# Start the pipeline
+	${cmd}
 
 fi
