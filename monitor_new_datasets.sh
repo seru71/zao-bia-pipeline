59d1823c4267aa1e03c83f1872efac682e2263ed
seru71 <pawel.sztromwasser@gmail.com>
2017-08-08 11:59:08 +0200

    finetuned monitoring script

--- monitor_new_datasets.sh
+++ monitor_new_datasets.sh
@@ -3,43 +3,52 @@
 # Script monitoring an input directory for new Illumina runs
 #
 
-# Directory to be monitored
-DIR=$1
-
 # Constants
 REPO=https://github.com/seru71/zao-bia-pipeline.git
 WORK_DIR=/ngs/prod/scratch
 LOG_FILE=/tmp/monitor_new_dataset.log
 
-# Check if new runfolder exists, and if the run is complete (marked with RTACommplete.txt)
-# Looks for file RTAComplete with modification date <1h from now
+function log {
+	echo -e `date -Iseconds`" - $1"
+}
 
+
+# Check directory to be monitored
+if [ -z $1 -o ! -e $1 ]; then
+    log "Missing or incorrect path to the directory to be monitored."
+    exit(1)
+fi
+
+export DIR=$1
+
+
+# Check if new runfolder exists, and if the run is complete (marked with RTAComplete.txt)
+# Looks for file RTAComplete with modification date <1h from now
 new=`find $DIR/*/RTAComplete.txt -mmin -60`
 
-echo `date -Iseconds`" - found: ${new}" >> ${LOG_FILE}
-if [ ! -z "${new}" ]; then
-	export RUN_FOLDER=`dirname ${new}`
-	export RUN_ID=`basename ${RUN_FOLDER}`
-	echo "Exporting RUN_FOLDER [${RUN_FOLDER}] and RUN_ID [${RUN_ID}]" >> ${LOG_FILE}
+if [ -z "${new}" ]; then
+	log "No new runs found in $DIR"
+    exit(0)
 fi
 
-# If the runfolder is complete, create pipeline config and start the pipeline
-if [ ! -z $RUN_ID ]; then
+log "Found: ${new}"
 
-	# Create pipeline config file
-	git clone ${REPO} ${WORK_DIR}/${RUN_ID}/pipeline
-#	sed 's///g' ${WORK_DIR}/${NEW_RUN}/pipeline/bia_pipeline.config.template > ${WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.config
-	cp ${WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.config.template ${WORK_DIR}/${RUN_ID}/pipeline.config
+# Configure workspace for the pipeline
+log "Configuring workspace for RUN_FOLDER [${RUN_FOLDER}] and RUN_ID [${RUN_ID}]"
+export RUN_FOLDER=`dirname ${new}`
+export RUN_ID=`basename ${RUN_FOLDER}`
+
+git clone ${REPO} ${WORK_DIR}/${RUN_ID}/pipeline
+cp ${WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.config.template ${WORK_DIR}/${RUN_ID}/pipeline.config
 	
-	cmd="${WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.py \
-		-s ${WORK_DIR}/${RUN_ID}/pipeline.config \
-		--run_folder ${RUN_FOLDER} \
-		-t complete_run \
-		-j8 -vvv &> ${WORK_DIR}/${RUN_ID}/pipeline_`date -Iseconds`.err"
+cmd="${WORK_DIR}/${RUN_ID}/pipeline/bia_pipeline.py \
+-s ${WORK_DIR}/${RUN_ID}/pipeline.config \
+--run_folder ${RUN_FOLDER} \
+-t complete_run \
+-j8 -vvv &> ${WORK_DIR}/${RUN_ID}/pipeline_`date -Iseconds`.err"
 
-	echo -e "Starting the pipeline with command:\n${cmd}" >> ${LOG_FILE}
 
-	# Start the pipeline
-	${cmd}
-
-fi
+# Start the pipeline
+log "Starting the pipeline with command: ${cmd}"
+${cmd}
+log "Pipeline finished with exit code: $?"
