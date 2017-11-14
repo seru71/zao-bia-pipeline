736ca43cdc62b303860514cc9a2c23eb27025885
seru71 <pawel.sztromwasser@gmail.com>
2017-11-14 13:48:09 +0100

    moved all logic to pipeline.py. tasks are pure task definitions

--- pipeline.py
+++ pipeline.py
@@ -15,35 +15,299 @@
 #
 
 
+# 8888888888888888888888888888888888888888
+#
+#   Per-sample config of the pipeline
+#
+# 8888888888888888888888888888888888888888
+
+
 
 class SampleTable:
     
     def get_sample_configs:
         # returns a dict with sample_ID : JSON 
+        return {}
 
+
+
+
+
+
+# 8888888888888888888888888888888888888888
+#
+#           C L I   a r g s
+#
+# 8888888888888888888888888888888888888888
+
+
+
+def check_mandatory_options (options, mandatory_options, helpstr):
+    """
+    Check if specified mandatory options have been defined
+    """
+    missing_options = []
+    for o in mandatory_options:
+        if not getattr(options, o):
+            missing_options.append("--" + o)
+
+    if not len(missing_options):
+        return
+
+    raise Exception("Missing mandatory parameter%s: %s.\n\n%s\n\n" %
+                    ("s" if len(missing_options) > 1 else "",
+                     ", ".join(missing_options),
+                     helpstr))
+    
+ def parse_cli_args():
+    
+    from optparse import OptionParser
+    import StringIO
+
+    parser = OptionParser(version="%prog 1.0", usage = "\n\n    %prog --settings bia_pipeline.config --run_folder RUN_FOLDER [--target_task TASK] [more_options]")
+    
+                                
+    #
+    #   general options: verbosity / logging
+    #
+    parser.add_option("-v", "--verbose", dest = "verbose",
+                      action="count", 
+                      help="Print more verbose messages for each additional verbose level.")
+    parser.add_option("-L", "--log_file", dest="log_file",
+                      metavar="FILE",
+                      type="string",
+                      help="Name and path of log file")
+
+
+    #
+    #   pipeline config and control
+    #
+    parser.add_option("-s", "--settings", dest="pipeline_settings",
+                        metavar="FILE",
+                        type="string",
+                        help="File containing all the settings for the analysis.")                  
+    parser.add_option("-r", "--run_folder", dest="run_folder",
+                        metavar="FILE",
+                        type="string",
+                        help="Directory with run's raw data as produced by Illumina sequencer")                  
+    parser.add_option("-t", "--target_tasks", dest="target_tasks",
+                        action="append",
+                        metavar="JOBNAME",
+                        type="string",
+                        help="Target task(s) of pipeline.")
+#    parser.add_option("-j", "--jobs", dest="jobs",
+#                        metavar="N",
+#                        type="int",
+#                        help="Allow N jobs (commands) to run simultaneously.")
+    parser.add_option("-n", "--just_print", dest="just_print",
+                        action="store_true", 
+                        help="Don't actually run any commands; just print the pipeline.")
+    parser.add_option("--flowchart", dest="flowchart",
+                        metavar="FILE",
+                        type="string",
+                        help="Don't actually run any commands; just print the pipeline "
+                             "as a flowchart.")
+
+    #
+    #   Less common pipeline options
+    #
+    parser.add_option("--key_legend_in_graph", dest="key_legend_in_graph",
+                        action="store_true",
+                        help="Print out legend and key for dependency graph.")
+    parser.add_option("--forced_tasks", dest="forced_tasks",
+                        action="append",
+                        metavar="JOBNAME",
+                        type="string",
+                        help="Pipeline task(s) which will be included even if they are up to date.")
+    parser.add_option("--rebuild_mode", dest="rebuild_mode",
+                        action="store_false", 
+                        help="gnu_make_maximal_rebuild_mode")
+    parser.add_option("--run_on_bcl_tile", dest="run_on_bcl_tile",
+                        type="string",                        
+                        help="Use only this tile when doing bcl2fastq conversion. For testing purposes.")
+    
+    parser.set_defaults(pipeline_settings=None, 
+                        jobs=1, verbose=0, 
+                        target_tasks=list(), forced_tasks=list(), 
+                        just_print=False, key_legend_in_graph=False,
+                        rebuild_mode=True, run_on_bcl_tile=None)
+    
+
+    # get help string
+    f = StringIO.StringIO()
+    parser.print_help(f)
+    helpstr = f.getvalue()
+    (options, remaining_args) = parser.parse_args()
+
+
+    mandatory_options = ['pipeline_settings', 'run_folder']
+    check_mandatory_options(options, mandatory_options, helpstr)
+ 
+    return options
+
+
+
+
+
+
+# 8888888888888888888888888888888888888888
+#
+#         L o g g i n g
+#
+# 8888888888888888888888888888888888888888
+
+
+def setup_std_logging (logger, log_file, verbose):
+    """
+    set up logging using programme options
+    """
+    class debug_filter(logging.Filter):
+        """
+        Ignore INFO messages
+        """
+        def filter(self, record):
+            return logging.INFO != record.levelno
+
+    class NullHandler(logging.Handler):
+        """
+        for when there is no logging
+        """
+        def emit(self, record):
+            pass
+
+    # We are interesting in all messages
+    logger.setLevel(logging.DEBUG)
+    has_handler = False
+
+    # log to file if that is specified
+    if log_file:
+        handler = logging.FileHandler(log_file, delay=False)
+        handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)6s - %(message)s"))
+        handler.setLevel(MESSAGE)
+        logger.addHandler(handler)
+        has_handler = True
+
+    # log to stderr if verbose
+    if verbose:
+        stderrhandler = logging.StreamHandler(sys.stderr)
+        stderrhandler.setFormatter(logging.Formatter("    %(message)s"))
+        stderrhandler.setLevel(logging.DEBUG)
+        if log_file:
+            stderrhandler.addFilter(debug_filter())
+        logger.addHandler(stderrhandler)
+        has_handler = True
+
+    # no logging
+    if not has_handler:
+        logger.addHandler(NullHandler())
+
+
+def setup_logging(log_file, verbosity):
+    import logging
+    import logging.handlers
+
+    MESSAGE = 15
+    logging.addLevelName(MESSAGE, "MESSAGE")
+    
+    module_name = "bia_pipeline"
+    logger = logging.getLogger(module_name)
+    setup_std_logging(logger, log_file, verbosity)
+
+    return logger
+
+    
+   
+# 88888888888888888888888888888888888888888
+#
+#              M A I N
+#
+# 88888888888888888888888888888888888888888
 
 
 if __name__ == '__main__':
 
 
-    # run bcl2fastq first?
+
+    options = parse_cli_args()
+
+
+    logger = setup_logging(options.log_file, options.verbose)
+    
+    #
+    #   Allow logging across Ruffus pipeline
+    #
+    def get_logger(logger_name, args):
+        return logger
+
+    from ruffus.proxy_logger import *
+    (logger_proxy, logging_mutex) = 
+        make_shared_logger_and_proxy(get_logger, logger.get_name(), {})
+        
+
+    #
+    # TODO
+    #
+    # run bcl2fastq and archive fastqs first?
+    #
+
+
 
     st = SampleTable()
     cfgs = st.get_sample_configs()
+    
+    
+    from pipeline import PipelineConfig, tasks
+    import drmaa
+    drmaa_session = drmaa.Session()
+    drmaa_session.initialize()
 
     # group by configs
+    cfgs_groups = {}
+    for k, v in cfgs.iteritems():
+        cfgs_groups[v] = cfgs_groups.setdefault(v, []).append(k)
+
+
+    for (cfg_group_idx, cfg_group) in enumerate(cfgs_groups.keys()):
+       
+        cfg = PipelineConfig(logger) 
+        cfg.set_runfolder(options.run_folder)
+        cfg.load_setting_from_file(options.pipeline_settings)
+        cfg.load_setting_from_JSON(cfg_group) # cfg_group is the group identifier and JSON string at the same time
     
-     for each group:
-        
-       cfg = PipelineConfig(logger)
-       cfg.set_runfolder(path)
-       cfg.load_from_file(settings_template)
-       cfg.load_from_JSON(groups_settings)
+
+        #
+        #
+        # task definitions should use e.g. cfg.reference
+        #
+        #
+
+        if options.just_print:
+            
+            print "SAMPLE_GROUP_" + cfg_group_idx + ":"
+            
+            pipeline_printout(sys.stdout, cfg.target_tasks, options.forced_tasks,
+                            gnu_make_maximal_rebuild_mode = options.rebuild_mode,
+                            verbose=options.verbose, verbose_abbreviated_path=0,
+                            checksum_level = 0)
+
+        elif options.flowchart:
+                        
+            pipeline_printout_graph (   open("SAMPLE_GROUP_"+cfg_group_idx+"_"+options.flowchart, "w"),
+                                    # use flowchart file name extension to decide flowchart format
+                                    #   e.g. svg, jpg etc.
+                                    os.path.splitext(options.flowchart)[1][1:],
+                                    cfg.target_tasks, options.forced_tasks,
+                                    gnu_make_maximal_rebuild_mode = options.rebuild_mode,
+                                    no_key_legend = not options.key_legend_in_graph)
+        else:        
+            pipeline_run(cfg.target_tasks, options.forced_tasks,
+                            multithread     = cfg.jobs,
+                            logger          = logger,
+                            verbose         = options.verbose,
+                            gnu_make_maximal_rebuild_mode = options.rebuild_mode,
+                            checksum_level  = 0)
+    
        
-       import ruffus
-       from pipeline import tasks
-       
-       pipeline_run(cfg.task, jobs=cfg.jobs, .... )
-       
-       
+    drmaa_session.exit()
+
        
